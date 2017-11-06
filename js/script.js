eb1d27b742d10b2a2b2a37118a5adbf29f517d12
micheledh <debrishue.michele@gmail.com>
2017-11-06 20:33:14 +0000

    fix http into https

--- js/script.js
+++ js/script.js
@@ -20,12 +20,12 @@ function loadData() {
 
     $greeting.text('So, you want to live at ' + address + '?');
 
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
     $body.append('<img class="bgimg" src="'+streetviewUrl+'">'); 
 
 
 
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+cityStr+'&sort=newest&api-key=f20bdbf515684a7bbab030ac0120eb49';
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+cityStr+'&sort=newest&api-key=f20bdbf515684a7bbab030ac0120eb49';
     $.getJSON(nytimesUrl, function (data) {
     	$nytHeaderElem.text('New York Times Articles About '+cityStr);
     	articles = data.response.docs;
@@ -38,7 +38,7 @@ function loadData() {
     });
 
 
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+cityStr+'&format=json&callback=wikiCallback';
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+cityStr+'&format=json&callback=wikiCallback';
     
     var wikiRequestTimeout = setTimeout(function(){
     	$wikiElem.text("failed to get wikipedia resources");
@@ -52,7 +52,7 @@ function loadData() {
     		var articleList = response[1];
     		for (var i = 0; i < articleList.length; i++){
     			articleStr = articleList[i];
-    			var url = 'http://en.wikipedia.org/wiki/'+articleStr;
+    			var url = 'https://en.wikipedia.org/wiki/'+articleStr;
     			$wikiElem.append('<li><a href="'+url+'">'+articleStr+'</a></li>');
     		};
     		clearTimeout(wikiRequestTimeout);
